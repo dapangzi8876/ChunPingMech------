cadba902fc444d6cbf6bdc1d09a17a231f6f62b9
dapangzi8876 <2277641074@qq.com>
2026-08-17 09:19:52 +0800

    8

--- Source/ChunPingMech/obj/Debug/netstandard2.1/ChunPingMech.AssemblyInfo.cs
+++ Source/ChunPingMech/obj/Debug/netstandard2.1/ChunPingMech.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChunPingMech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e759dbcd233a44b40ebc74f022ea14a90e70e77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChunPingMech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChunPingMech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
